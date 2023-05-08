3b67a44bd4b8db0c2135ff96d92ed29a08614c55
umairali435 <53268680+umairali435@users.noreply.github.com>
2023-05-08 02:23:13 +0000

    bubble sort

--- Main.java
+++ Main.java
@@ -2,10 +2,23 @@ import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
     int[] arr ={1,4,3,6,2,5};
-    selectionSort(arr);
+    inserationSort(arr);
     System.out.println(Arrays.toString(arr));
   }
 
+  static void inserationSort(int[] arr){
+    for(var i=0; i< arr.length-1; i++){
+      for(var j=i+1; j > 0; j--){
+        if(arr[j] < arr[j-1]){
+          int temp = arr[j-1];
+          arr[j-1] = arr[j];
+          arr[j] = temp;
+        }else{
+          break;
+        }
+      }
+    }
+  }
   static void selectionSort(int[] arr){
     for(int i=0; i < arr.length; i++){
       int last = arr.length -i - 1; // 5
